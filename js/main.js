1d6468d672c91af79bc8150394fd0f6cfff303b0
Павел Рафеев <rafeev@office.bitrix.ru>
2017-08-23 18:01:39 +0200

    main complete

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 window.onload = function()
 {
-	
+	function toggleMenu()
+	{
+		var togllerBlock = document.querySelector('[data-role="menu-show"]');
+		togllerBlock.addEventListener('click', function()
+			{
+				this.classList.toggle('is-active');
+				document.body.classList.toggle('show-menu');
+				
+			}, false)
+	};
+
+
+
+	toggleMenu();
 }
