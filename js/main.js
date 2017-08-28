8d96d8fafc10768eb14de891b0b3b903c3a8f936
Pavel Rafeev <rafeev.pavel@gmail.com>
2017-08-28 11:20:57 +0200

    Dimich

--- js/main.js
+++ js/main.js
@@ -13,17 +13,41 @@ window.onload = function()
 
 	function showPopup()
 	{
-		var popupLink = document.querySelectorAll('[data-role="show-popup"]');
-		for(var i = 0; i < popupLink.length; i++)
-		{
-			popupLink[i].addEventListener('click', function()
+		var showPopupLink = document.querySelectorAll('[data-role="show-popup"]');
+		var popupClose = document.querySelectorAll('[data-role="popup-close"]');
+		var overlay = document.querySelector('[data-role="overlay"]');
+
+		overlay.addEventListener('click', function()
+			{
+				var popups = document.querySelectorAll('.popup');
+				this.classList.remove('overlay-show');
+				for(var i = 0; i < popups.length; i++)
 				{
-					var attrFor = this.getAttribute('for');
-					var popup = document.getElementById(attrFor);
-					// console.log(attrFor);
-					popup ? popup.classList.toggle('popup-show') : (return);
+					popups[i].classList.remove('popup-show');
+				}
+			}, false)
+
+		for(var i = 0; i < showPopupLink.length; i++)
+		{
+			showPopupLink[i].addEventListener('click', function()
+				{
+					var attr = this.getAttribute('for');
+					var popup = document.getElementById(attr);
+					popup ?	popup.classList.toggle('popup-show') : null;
+					popup ? overlay.classList.toggle('overlay-show') : null;
 				}, false)
-		}		
+		}
+
+		for(var i = 0; i < popupClose.length; i++)
+		{
+			popupClose[i].addEventListener('click', function()
+				{
+					var attr = this.getAttribute('for');
+					var popup = document.getElementById(attr);
+					popup ?	popup.classList.toggle('popup-show') : null;
+					popup ? overlay.classList.toggle('overlay-show') : null;
+				}, false)
+		}
 	}
 
 	showPopup();
